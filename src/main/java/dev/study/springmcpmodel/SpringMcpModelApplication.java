b663b203b9c25616a52dac909d623eac616933cc
KIM JAE HYUK <rlawogur816@nextorm.com>
2025-04-04 16:52:51 +0900

    feature: Mcp Server 구현 gradle 의존성 변경

--- src/main/java/dev/study/springmcpmodel/SpringMcpModelApplication.java
+++ src/main/java/dev/study/springmcpmodel/SpringMcpModelApplication.java
@@ -1,8 +1,14 @@
 package dev.study.springmcpmodel;
 
+import dev.study.springmcpmodel.tool.RedisStaticsTool;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbacks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SpringMcpModelApplication {
@@ -11,4 +17,8 @@ public class SpringMcpModelApplication {
         SpringApplication.run(SpringMcpModelApplication.class, args);
     }
 
+    @Bean
+    public List<ToolCallback> redisTools(RedisStaticsTool redisStaticsTool){
+        return List.of(ToolCallbacks.from(redisStaticsTool));
+    }
 }
--- src/main/java/dev/study/springmcpmodel/tool/RedisStaticsTool.java
+++ src/main/java/dev/study/springmcpmodel/tool/RedisStaticsTool.java
@@ -1,5 +1,7 @@
 package dev.study.springmcpmodel.tool;
 
+import io.modelcontextprotocol.server.McpSyncServer;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.data.redis.connection.RedisConnection;
@@ -7,11 +9,12 @@ import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.stringtemplate.v4.ST;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -20,41 +23,37 @@ public class RedisStaticsTool {
     private final RedisTemplate<String, String> redisTemplate;
 
     @Tool(name = "find_key_without_ttl", description = "Find Redis key without TTL")
-    public Map<String, Object> getRedisKeyWithOutTTL() {
+    public List<String> getRedisKeyWithOutTTL() {
         ScanOptions scanOptions = ScanOptions.scanOptions().match("*").count(1000).build();
-        List<String> keys = redisTemplate.execute((RedisConnection redisConnection) -> {
+        // TTL이 -1인 경우 만료 시간이 설정되지 않은 키입니다
+        return redisTemplate.execute((RedisConnection redisConnection) -> {
             List<String> keyList = new ArrayList<>();
             try (Cursor<byte[]> cursor = redisConnection.keyCommands().scan(scanOptions)) {
                 while (cursor.hasNext()) {
-                    keyList.add(new String(cursor.next()));
+                    String key = new String(cursor.next());
+                    // TTL이 -1인 경우 만료 시간이 설정되지 않은 키입니다
+                    Long ttl = redisTemplate.getExpire(key);
+                    if (ttl == -1) {
+                        keyList.add(key);
+                    }
                 }
             }
             return keyList;
         }, true);
-
-        return Map.of(
-                "jsonrpc", "2.0",
-                "result", Objects.requireNonNull(keys)
-        );
     }
 
     @Tool(name = "find_key", description = "Find Redis key")
-    public Map<String, Object> getRedisKey(String key) {
+    public Map<String ,String> getRedisKey(String key) {
         String value = redisTemplate.opsForValue().get(key);
         if (value == null) {
-            return Map.of(
-                    "jsonrpc", "2.0",
-                    "error", "Key not found"
-            );
+            return Map.of();
         }
 
-        return Map.of(
-                "jsonrpc", "2.0",
-                "result", String.format("Key: %s, Value: %s", key, value)
-        );
+        return Map.of(key, value);
     }
 
     @Tool(name = "delete_key", description = "Delete Redis key")
+    @Transactional
     public boolean deleteRedisKey(String key) {
         return redisTemplate.delete(key);
     }
@@ -71,6 +70,7 @@ public class RedisStaticsTool {
     }
 
     @Tool(name = "create_key", description = "Create Redis key")
+    @Transactional
     public boolean createRedisKey(String key, String value) {
         try {
             redisTemplate.opsForValue().set(key, value);
